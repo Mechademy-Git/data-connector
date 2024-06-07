9ce078ee8e58c972964d79c1abd4b70dfadf95eb
Shubham Sharma <shubham.sharma@mechademy.com>
2024-06-07 17:32:25 +0530

    remove: server execution

--- startup.ps1
+++ startup.ps1
@@ -80,12 +80,5 @@ Start-Sleep -Seconds 2
 .\nssm.exe set mechademy-celery-beat AppStderr "$ProjectPath\celery_beat_error.log"
 .\nssm.exe start mechademy-celery-beat
 
-Start-Sleep -Seconds 2
-
-.\nssm.exe install mechademy-server "$ProjectPath\start_server.bat"
-.\nssm.exe set mechademy-server AppDirectory "$ProjectPath"
-.\nssm.exe set mechademy-server AppStdout "$ProjectPath\server.log"
-.\nssm.exe set mechademy-server AppStderr "$ProjectPath\server_error.log"
-.\nssm.exe start mechademy-server
 
 Write-Output "Scheduler started successfully :)"
