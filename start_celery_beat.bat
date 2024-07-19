163e786dcb39b643236966bfdb9cb02cac129702
Shubham Sharma <shubham.sharma@mechademy.com>
2024-07-19 15:55:16 +0530

    delete unnecessary files

--- start_celery_beat.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Program Files\Mechademy\data-connector-main
-call .venv\Scripts\activate
-poetry run celery -A src.celery.celery beat --loglevel=info
--- start_server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Program Files\Mechademy\data-connector-main
-call .venv\Scripts\activate
-poetry run uvicorn src.main:app --host 0.0.0.0 --port 8000
